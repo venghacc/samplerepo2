6c81be73db36a97ac581ae2e951ac64b798f11be
venghacc <venkirkr@gmail.com>
2017-12-19 19:06:34 +0530

    added comment
    aded comment

---
+++ src/main/java/com/example/helloendpoints/Constants.java
@@ -0,0 +1,13 @@
+package com.example.helloendpoints;
+
+/**
+ * aaaaContains the client IDs and scopes for allowed clients consuming the helloworld API.
+ */
+public class Constants {
+  public static final String WEB_CLIENT_ID = "203923117488-g1njts1k5ual9tmvm9d82c6grsf1b2pe.apps.googleusercontent.com";
+  public static final String ANDROID_CLIENT_ID = "replace this with your Android client ID";
+  public static final String IOS_CLIENT_ID = "replace this with your iOS client ID";
+  public static final String ANDROID_AUDIENCE = WEB_CLIENT_ID;
+
+  public static final String EMAIL_SCOPE = "https://www.googleapis.com/auth/userinfo.email";
+}
